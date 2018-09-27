6cc0a34560774f313b1562f574f0e90e637eb994
Angelica Winkler <carowink3@gmail.com>
2018-09-27 16:38:36 -0400

    final

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,14 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+  has_many :allergies
+  has_many :users, through: :allergies
+
+  def self.ranked_allergies
+    ranked = Ingredient.all.sort_by do |ing|
+      ing.allergies.count
+    end
+    ranked.reverse
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_160320) do
+ActiveRecord::Schema.define(version: 2018_09_27_201532) do
+
+  create_table "allergies", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
